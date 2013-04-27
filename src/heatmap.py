5cda8aca7bf0ec7f7e5e6245a3daa1fb3ec42f18
unknown <SimonCB765@gmail.com>
2013-04-28 00:44:50 +0100

    Updated heatmap and new histogram

--- src/heatmap.py
+++ src/heatmap.py
@@ -1,26 +1,22 @@
 import argparse
 from matplotlib import cm
 from matplotlib import colors
+import matplotlib.pyplot as plt
 import numpy as np
-import pylab
 import sys
 
 def commandLine(args):
 	"""
-	uses a -f argument to indicate the file input type
 	"""
 
 	parser = argparse.ArgumentParser(description='Process the command line input for creating a heatmap.')
 	parser.add_argument('inputFile', help='an input file containing the values for the heatmap')
-	parser.add_argument('-f', '--format', default='tsv', choices=['tsv', 'csv'], help='the format of the input file')
+	parser.add_argument('-d', '--delimiter', default='\t', help='the delimiter for the lines of the input file')
 	parser.add_argument('--normMin', default=None, help='the minimum value used for the normalisation')
 	parser.add_argument('--normMax', default=None, help='the maximum value used for the normalisation')
 	parser.add_argument('-c', '--colorMap', default='jet', choices=['jet'], help='the color map to use')
 	args = parser.parse_args()
-	delimiter = '\t'
-	if args.format == 'csv':
-		delimiter = ','
-	parseFile(args.inputFile, delimiter=delimiter, normMin=args.normMin, normMax=args.normMax, colorMap=args.colorMap)
+	parseFile(args.inputFile, delimiter=args.delimiter, normMin=args.normMin, normMax=args.normMax, colorMap=args.colorMap)
 
 def parseFile(inputFile, delimiter='\t', normMin=None, normMax=None, colorMap='jet'):
 	"""
@@ -30,17 +26,28 @@ def parseFile(inputFile, delimiter='\t', normMin=None, normMax=None, colorMap='j
 	graphGeneration(similarities, normMin=normMin, normMax=normMax, colorMap=colorMap)
 		
 
-def graphGeneration(similarities, normMin=None, normMax=None, colorMap='jet'):
+def graphGeneration(similarities, divisions=[], normMin=None, normMax=None, colorMap='jet'):
 	"""
 	"""
 
 	numberOfIndividuals = len(similarities)
-	xAxis = np.array(range(numberOfIndividuals + 1))
-	yAxis = np.array(range(numberOfIndividuals + 1))
 
 	norm = colors.Normalize(vmin=normMin, vmax=normMax)
-	pylab.pcolormesh(xAxis, yAxis, similarities, cmap=cm.jet, norm=norm)
-	pylab.show()
+	plt.pcolormesh(similarities, norm=norm)
+	plt.set_cmap(colorMap)
+	plt.colorbar()
+	plt.plot([0, numberOfIndividuals], [0, numberOfIndividuals], color='k')
+	for i in divisions:
+		plt.plot([i, i], [0, numberOfIndividuals], color='k', linewidth=2)
+		plt.plot([0, numberOfIndividuals], [i, i], color='k', linewidth=2)
+	plt.xlim(xmax=numberOfIndividuals)
+	plt.ylim(ymax=numberOfIndividuals)
+	axes = plt.gca()
+	xAxis = axes.get_xaxis()
+	xAxis.set_visible(False)
+	yAxis = axes.get_yaxis()
+	yAxis.set_visible(False)
+	plt.show()
 
 if __name__ == '__main__':
 	commandLine(sys.argv)
