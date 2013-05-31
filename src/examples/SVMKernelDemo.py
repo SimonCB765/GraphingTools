c103200a50c5858d127cc61b876894059721be48
unknown <SimonCB765@gmail.com>
2013-05-31 13:10:33 +0100

    Final functional update for SVM kernel demo

--- src/examples/SVMKernelDemo.py
+++ src/examples/SVMKernelDemo.py
@@ -15,6 +15,7 @@ import surface3D
 
 def main(figureSaveLocation):
     """
+    The hyperplane in the 3D plot sometimes appears without its edge colour. If you want the edge colour, then run the script until it appears.
     """
 
     # Define the image size.
@@ -25,17 +26,76 @@ def main(figureSaveLocation):
     maxZ = 5
     minZ = -5
 
+    # Define the width of the circular border.
+    circleWidth = 2.2
+
     # Setup the scatter data for class 1.
-    class1XValues = np.array([0.0, 0.5, 0.5, -0.5, -0.5])
-    class1YValues = np.array([0.0, 0.5, -0.5, 0.5, -0.5])
+    class1XValues = np.array([
+        -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1,
+        -1.0, -0.9, -0.8, -0.8, -0.7, -0.6, -0.5, -0.5, -0.4, -0.3, -0.2, -0.2, -0.1,
+        0.0, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+        1.0, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8
+        ])
+    class1YValues = np.array([
+        0.1, -0.2, 0.4, -0.4, 0.3, 0.5, -1.0, 0.4,
+        0.3, 1.1, 1.35, -0.2, 0.5, -0.9, 1.3, 0.3, 1.6, -1.2, -0.5, -1.6, 0.7,
+        1.3, 0.4, -1.3, 0.6, 0.1, -0.5, 1.3, 0.4, -1.1, -0.8, -1.5,
+        1.1, 0.3, -0.3, 0.9, -0.9, 1.2, -0.6, 0.3, 0.6, -0.2
+        ])
     class1ZValues = np.array([maxZ - math.sqrt(i**2 + j**2) for i, j in zip(class1XValues, class1YValues)])
     class1Color = '#0000FF'  # Blue
     class1Size = 30
 
     # Setup the scatter data for class 2.
-    class2XValues = np.array([-3.0, -3.0, 3.0, 3.0])
-    class2YValues = np.array([-3.0, 3.0, -3.0, 3.0])
-    class2ZValues = np.array([maxZ - math.sqrt(i**2 + j**2) for i, j in zip(class2XValues, class2YValues)])
+    class2XValues = np.array([
+        -0.0, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9,
+        -0.0, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9,
+        -1.0, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.7, -1.8, -1.9,
+        -1.0, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.7, -1.8, -1.9,
+        -2.0, -2.0, -2.1, -2.2, -2.3, -2.3, -2.4, -2.4, -2.4, -2.5, -2.5, -2.6, -2.7, -2.8, -2.8, -2.8, -2.9,
+        -2.0, -2.0, -2.1, -2.2, -2.3, -2.3, -2.4, -2.4, -2.4, -2.5, -2.5, -2.6, -2.7, -2.8, -2.8, -2.8, -2.9,
+        -3.0, -3.0, -3.1, -3.2, -3.2, -3.3, -3.4, -3.4, -3.4, -3.5, -3.5, -3.6, -3.7, -3.8, -3.9,
+        -3.0, -3.0, -3.1, -3.2, -3.2, -3.3, -3.4, -3.4, -3.4, -3.5, -3.5, -3.6, -3.7, -3.8, -3.9,
+        -4.0, -4.0, -4.1, -4.1, -4.2, -4.2, -4.3, -4.4, -4.5, -4.6, -4.6, -4.7,
+        -4.0, -4.0, -4.1, -4.1, -4.2, -4.2, -4.3, -4.4, -4.5, -4.6, -4.6, -4.7,
+        -4.0, -4.2, -4.6,
+        0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+        0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+        1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.7, 1.8, 1.9,
+        1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.7, 1.8, 1.9,
+        2.0, 2.0, 2.1, 2.2, 2.3, 2.3, 2.4, 2.4, 2.4, 2.5, 2.5, 2.6, 2.7, 2.8, 2.8, 2.8, 2.9,
+        2.0, 2.0, 2.1, 2.2, 2.3, 2.3, 2.4, 2.4, 2.4, 2.5, 2.5, 2.6, 2.7, 2.8, 2.8, 2.8, 2.9,
+        3.0, 3.0, 3.1, 3.2, 3.2, 3.3, 3.4, 3.4, 3.4, 3.5, 3.5, 3.6, 3.7, 3.8, 3.9,
+        3.0, 3.0, 3.1, 3.2, 3.2, 3.3, 3.4, 3.4, 3.4, 3.5, 3.5, 3.6, 3.7, 3.8, 3.9,
+        4.0, 4.0, 4.1, 4.1, 4.2, 4.2, 4.3, 4.4, 4.5, 4.6, 4.6, 4.7,
+        4.0, 4.0, 4.1, 4.1, 4.2, 4.2, 4.3, 4.4, 4.5, 4.6, 4.6, 4.7,
+        4.0, 4.2, 4.6
+        ])
+    class2YValues = np.array([
+        3.0, 4.2, 4.7, 2.6, 3.5, 3.9, 4.4, 2.9, 4.1, 3.7,
+        -3.0, -4.2, -4.7, -2.6, -3.5, -3.9, -4.4, -2.9, -4.1, -3.7,
+        2.4, 3.8, 4.2, 3.1, 2.7, 4.7, 3.4, 2.5, 2.0, 4.3, 3.6,
+        -2.4, -3.8, -4.2, -3.1, -2.7, -4.7, -3.4, -2.5, -2.0, -4.3, -3.6,
+        1.7, 2.6, 4.5, 3.2, 1.9, 4.1, 0.6, 1.0, 2.4, 1.5, 3.8, 2.6, 4.7, 0.3, 3.3, 0.7, 4.0,
+        -1.7, -2.6, -4.5, -3.2, -1.9, -4.1, -0.6, -1.0, -2.4, -1.5, -3.8, -2.6, -4.7, -0.3, -3.3, -0.7, -4.0,
+        0.7, 2.0, 3.4, 2.5, 1.6, 4.6, 0.4, 1.1, 2.1, 0.9, 3.7, 2.9, 4.3, 1.7, 4.1,
+        -0.7, -2.0, -3.4, -2.5, -1.6, -4.6, -0.4, -1.1, -2.1, -0.9, -3.7, -2.9, -4.3, -1.7, -4.1,
+        0.9, 3.6, 2.8, 1.3, 2.0, 4.1, 2.3, 0.7, 4.5, 3.2, 1.6, 3.9,
+        -0.9, -3.6, -2.8, -1.3, -2.0, -4.1, -2.3, -0.7, -4.5, -3.2, -1.6, -3.9,
+        0.4, 0.0, 0.2,
+        3.0, 4.2, 4.7, 2.6, 3.5, 3.9, 4.4, 2.9, 4.1, 3.7,
+        -3.0, -4.2, -4.7, -2.6, -3.5, -3.9, -4.4, -2.9, -4.1, -3.7,
+        2.4, 3.8, 4.2, 3.1, 2.7, 4.7, 3.4, 2.5, 2.0, 4.3, 3.6,
+        -2.4, -3.8, -4.2, -3.1, -2.7, -4.7, -3.4, -2.5, -2.0, -4.3, -3.6,
+        1.7, 2.6, 4.5, 3.2, 1.9, 4.1, 0.6, 1.0, 2.4, 1.5, 3.8, 2.6, 4.7, 0.3, 3.3, 0.7, 4.0,
+        -1.7, -2.6, -4.5, -3.2, -1.9, -4.1, -0.6, -1.0, -2.4, -1.5, -3.8, -2.6, -4.7, -0.3, -3.3, -0.7, -4.0,
+        0.7, 2.0, 3.4, 2.5, 1.6, 4.6, 0.4, 1.1, 2.1, 0.9, 3.7, 2.9, 4.3, 1.7, 4.1,
+        -0.7, -2.0, -3.4, -2.5, -1.6, -4.6, -0.4, -1.1, -2.1, -0.9, -3.7, -2.9, -4.3, -1.7, -4.1,
+        0.9, 3.6, 2.8, 1.3, 2.0, 4.1, 2.3, 0.7, 4.5, 3.2, 1.6, 3.9,
+        -0.9, -3.6, -2.8, -1.3, -2.0, -4.1, -2.3, -0.7, -4.5, -3.2, -1.6, -3.9,
+        0.4, 0.0, 0.2
+        ])
+    class2ZValues = np.array([max(maxZ - math.sqrt(i**2 + j**2), 0.0) for i, j in zip(class2XValues, class2YValues)])
     class2Color = '#FF0000'  # Red
     class2Size = 30
 
@@ -73,7 +133,7 @@ def main(figureSaveLocation):
     scatterplot.graphGeneration(scatterXValues, scatterYValues, currentFigure=currentFigure, colors=colors, sizes=sizes)
     plotWithBoundary.set_xlim(left=minX, right=maxX)
     plotWithBoundary.set_ylim(bottom=minY, top=maxY)
-    currentFigure.gca().add_artist(plt.Circle((0, 0), 2.0, fill=False))
+    currentFigure.gca().add_artist(plt.Circle((0, 0), circleWidth, fill=False))
 
     # Make all the tick marks invisible, and label the x axes.
     labels = ['(c)', '(b)', '(a)']
@@ -83,8 +143,7 @@ def main(figureSaveLocation):
         setLabels(ax, xLabel=currentLabel)
         ax.xaxis.set_label_coords(0.5, -0.025)
 
-    plt.savefig(figureSaveLocation + '2D', bbox_inches=0, transparent=True)
-    plt.show()
+    plt.savefig(figureSaveLocation + '2D', bbox_inches='tight', transparent=True)
 
     # Create the 3D graph showing the new division.
     currentFigure = plt.figure()
@@ -92,21 +151,22 @@ def main(figureSaveLocation):
     gs.update(left=0, right=1, bottom=0, top=1, wspace=0.05)#, hspace=0.05)
     scatter3D = plt.subplot(gs[1:-1, 1:-1], projection='3d')
     scatter3D.axis('scaled')
+    scatter3D.set_xlim3d(minX, maxX)
+    scatter3D.set_ylim3d(minY, maxY)
+    scatter3D.set_zlim3d(0, maxZ)
     scatterplot3D.graphGeneration(scatterXValues, scatterYValues, scatterZValues, currentFigure=currentFigure, colors=colors, sizes=sizes)
     X = np.arange(minX, maxX + 1)
     Y = np.arange(minY, maxY + 1)
     X, Y = np.meshgrid(X, Y)
-    Z = np.zeros(np.shape(X)) + (maxZ - 2)
-    surface3D.graphGeneration([X], [Y], [Z], currentFigure=currentFigure, color=['black'], edgeColor=['black'], transparency=[0.1])
-    scatter3D.set_xlim3d(minX, maxX)
-    scatter3D.set_ylim3d(minY, maxY)
-    scatter3D.set_zlim3d(0, maxZ)
+    Z = np.zeros(np.shape(X)) + (maxZ - circleWidth)
+    surface3D.graphGeneration([X], [Y], [Z], currentFigure=currentFigure, color=['black'], edgeColor=['black'], transparency=[0.2])
     removeTickMarks(scatter3D, xAxis=True, yAxis=True)
     for a in scatter3D.w_zaxis.get_ticklines()+scatter3D.w_zaxis.get_ticklabels():
 	    # Used to remove the z axis ticks and tick labels.
         a.set_visible(False)
+    scatter3D.view_init(2, 104)  # Rotate figure to nicer angle.
 
-    plt.savefig(figureSaveLocation + '3D', bbox_inches=0, transparent=True)
+    plt.savefig(figureSaveLocation + '3D', bbox_inches='tight', transparent=True)
     plt.show()
 
 
